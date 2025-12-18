9d5d490b4eea6cf8ee6d68437c81cac529727305
AyushKrishnaJ <ayush_b240151cs@nitc.ac.in>
2025-12-18 21:01:16 +0530

    Seller Details About section template

--- src/components/SellerDetails.jsx
+++ src/components/SellerDetails.jsx
@@ -25,8 +25,8 @@ function SdImage() {
         <div className="sd-image-container">
             <img className="sd-image" src={thumb} alt="thumbnail"/>
             <div className="sd-lastupdated">
-                <img className="sd-clock" src={clockSvg} alt="clock"/>
-                    Updated 12 minutes ago.
+                <img className="sd-lastupdated-clock" src={clockSvg} alt="clock"/>
+                <p className="sd-lastupdated-text">Updated 12 minutes ago.</p>
             </div>
         </div>
     )
@@ -36,7 +36,15 @@ function SdImage() {
 function SdDetails() {
     return(
         <div className="sd-details">
-            Details
+            <div className="sd-details-header">
+                <div className="sd-details-name">Rahul Kumar</div>
+                <div className="sd-details-loc">Block A Room 204</div>
+                <div className="sd-details-open-closed">open</div>
+            </div>
+            <div className="sd-details-about">
+                <p className="sd-details-about-header">About</p>
+                <p className="sd-details-about-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, nulla cumque ab velit iste et sequi vel molestiae quos consequatur hic ullam, facilis nemo fuga, praesentium temporibus! Reiciendis, cum veritatis.</p>
+            </div>
         </div>
     )
 }
